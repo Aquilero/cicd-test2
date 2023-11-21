b429581405bd26ac9d44341151f3033367ba7ecf
Deniz <deniz.durmaz@docc.techstarter.de>
2023-11-21 14:08:43 +0100

    tfstate outsources bucket

--- infra/versions.tf
+++ infra/versions.tf
@@ -7,6 +7,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         	   = "ue139-testbucket"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-central-1"
+    encrypt        	   = true
+    # dynamodb_table = "mycomponents_tf_lockid"
+    profile           = "sandbox95"
+  }
 }
 
 provider "aws" {
