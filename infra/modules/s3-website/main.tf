e971b03e0b767b394dc0ec76d669b26ac17ccdca
Deniz <deniz.durmaz@docc.techstarter.de>
2023-11-21 12:00:23 +0100

    fix{github/workflows}: change & profile

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -28,3 +28,22 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+
+# resource "aws_s3_bucket" "name" {
+  
+# }
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": "s3:*",
+#       "Resource": [
+#         "arn:aws:s3:::my-website-bucket-fabio--23112023/*",
+#         "arn:aws:s3:::my-website-bucket-fabio--23112023"
+#       ]
+#     }
+#   ]
+# }
